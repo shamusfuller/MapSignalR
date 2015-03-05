3eabb4f2d04a32424122b692fd257063fc66b54e
shamus <shamusfuller@users.noreply.github.com>
2015-03-04 19:55:31 -0600

    add mising commit?

--- MapSignalR/Layer.cs
+++ MapSignalR/Layer.cs
@@ -8,5 +8,24 @@ namespace MapSignalR
 {
     public class Layer
     {
+                //    var shadedRelief1 = new Cesium.WebMapTileServiceImageryProvider({
+                //url: 'http://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/WMTS/',
+                //layer: 'USGSShadedReliefOnly',
+                //style: 'default',
+                //format: 'image/jpeg',
+                //tileMatrixSetID: 'default028mm',
+                //// tileMatrixLabels : ['default028mm:0', 'default028mm:1', 'default028mm:2' ...],
+                //maximumLevel: 19,
+                //credit: new Cesium.Credit('U. S. Geological Survey')
+        public string Url { get; set; }
+        public string LayerName { get; set; }
+        public string Style { get; set; }
+        public string Format { get; set; }
+        public string TileMatrixSetID { get; set; }
+        //public string[] TileMatrixLabels { get; set; }
+        public string MaximumLevel { get; set; }
+        public string Credit { get; set; }
+        
+
     }
 }
