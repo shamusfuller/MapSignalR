cff7657be3b19a118c5cf1e89bbe521ac057bfb6
shamus <shamusfuller@users.noreply.github.com>
2015-03-04 19:35:23 -0600

    initial layer load

---
+++ MapSignalR/MapHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapSignalR
+{
+    public class MapHub: Hub
+    {
+        public async Task Test()
+        {
+            //await Task.Delay(0);
+            await Clients.All.test(); // this call has different signature than this.Test
+        }
+    }
+}
