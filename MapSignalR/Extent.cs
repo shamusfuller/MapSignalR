f99709882fb2483bec4fe0d7fc008326847c878c
shamus <shamusfuller@users.noreply.github.com>
2015-03-18 18:02:15 -0500

    add Map and Extent

---
+++ MapSignalR/Extent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapSignalR
+{
+    public class Extent
+    {
+        public decimal East { get; set; }
+        public decimal North { get; set; }
+        public decimal South { get; set; }
+        public decimal West { get; set; }
+    }
+}
---
+++ MapSignalR/Map.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapSignalR
+{
+    public class Map
+    {
+        public Extent Extents { get; set; }
+        public IEnumerable<Layer> Layers { get; set; }
+        public string Name { get; set; }
+    }
+}
